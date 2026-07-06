06ea8d52c5c3db148f9bd37424ab537c1e9e4548
mladenm11111 <mladen.m014@gmail.com>
2026-07-06 02:16:38 +0200

    Dodat Docker

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,7 @@
 <?php
-const DB_HOST = 'localhost';
+const DB_HOST = 'db';
 const DB_USER = 'root';
-const DB_PASS = '';
+const DB_PASS = 'root';
 const DB_NAME = 'suplement_store';
 const DB_PORT = '3306';
-const BASE_URL = 'http://localhost/suplement-store/public/';
+const BASE_URL = 'http://localhost:8080/';
